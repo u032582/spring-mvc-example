670b43f8d8d9a51ffb3fbe9b8cbab0ec46f61609
u032582 <u032502@mokumet.com>
2022-03-31 06:47:04 +0900

    work

--- src/main/java/com/example/SampleRestController.java
+++ src/main/java/com/example/SampleRestController.java
@@ -1,36 +1,44 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.User;
 
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "group1")
 @RestController
+@RequestMapping("rest")
 public class SampleRestController {
 	@Autowired
 	private UserRepository userRepository;
 
 	/**
-	 * テストメソッドです.
+	 * ユーザーリストを取得します。
 	 * 
-	 * @param name 名前を指定します
-	 * @return {@link User}を返却します
+	 * @return Userのリスト
 	 */
 	@Transactional
-	@PostMapping("/test")
+	@GetMapping("/users")
 	@ApiResponse(responseCode = "200", description = "成功時", content = @Content(mediaType = "application/json", schema = @Schema(implementation = User.class)))
 	@ApiResponse(responseCode = "500", description = "失敗時")
-	public User test(@RequestBody User user) {
-		return userRepository.findById(user.getEmail()).get();
+	public List<User> users() {
+		var ite = userRepository.findAll().iterator();
+		List<User> list = new ArrayList<User>();
+		while (ite.hasNext()) {
+			list.add(ite.next());
+		}
+		return list;
 	}
 }
